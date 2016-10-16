98e261971394f8968d28f1f7cd4a7fa6a0c65ba7
Tramel Jones <tramel.jones@gmail.com>
2016-10-16 18:42:06 -0500

    disable forgery on development

--- webapp/config/environments/development.rb
+++ webapp/config/environments/development.rb
@@ -31,7 +31,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
-
+  # request origins 
+  config.action_cable.disable_request_forgery_protection = true
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
